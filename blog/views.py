d87682f1459bd71d9c66b01066540db4ea49c887
William Wernsen <we.wernsen@gmail.com>
2016-02-17 21:31:04 +0100

    Added product website

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .push import Push
 from .forms import PostForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
