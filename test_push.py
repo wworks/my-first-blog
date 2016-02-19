0666d3b2dcb6243281c821f25db84dbbd718fd50
William Wernsen <we.wernsen@gmail.com>
2016-02-19 16:49:41 +0100

    clean

--- test_push.py
+++
@@ -1,9 +0,0 @@
-from gcm import *
-
-gcm = GCM("AIzaSyAhXO4yU0eTXc3cfoMVvsy0WA3T8uTT_H4")
-data = {'the_message': 'You have x new friends', 'param2': 'value2'}
-
-reg_id = 'APA91bH6HIUlPVEBsMAmgVIDCrB6kwl8bdO6Oer_TrZEpOjolSsWIbMnuo8hmpBdvvTrr2srX1j70WSRZaSKrXIWHs89-_cTqRoKMETP3Nmo7EB6wt13A-HxYUfPRz_uqJ7nVzlZBNKw'
-
-gcm.plaintext_request(registration_id=reg_id, data=data)
-
